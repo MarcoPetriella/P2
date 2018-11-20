2abc943107284e9f2e68437f2f1af8c5f263aaaf
“Marco <marcopetriella@gmail.com>
2018-11-20 14:59:46 -0300

    arduino tipo clase

--- Arduino/v3/comunicacion.py
+++ Arduino/v3/comunicacion.py
@@ -4,6 +4,7 @@ Created on Tue Nov 13 14:18:03 2018
 
 @author: Marco
 """
+#%%
 
 import win32pipe, win32file,time,struct
 import numpy as np
@@ -20,13 +21,20 @@ import struct
 import array
 import threading
 
-arduino = serial.Serial('COM5', 2*9600, timeout=0.5)
+arduino = serial.Serial('COM4', 2*9600, timeout=0.5)
 arduino.set_buffer_size(rx_size = 8*1000, tx_size = 8*1000)
 
-cant_variables= 10
+cant_chunk_rec = 1
 sub_chunk_plot = 1
-buffer_in_data = np.zeros([1000,cant_variables])
-buffer_in_timestamp = [None]*1000
+cant_variables= 11
+buffer_in_data = np.zeros([10000,cant_variables])
+buffer_in_timestamp = [None]*10000
+
+setpoint = 2000
+kp = 0.1
+ki = 0.2
+kd = 0.3
+isteps = 20.   
 
 evento_salida = threading.Event()
 semaphore1 = threading.Semaphore(0) 
@@ -36,75 +44,45 @@ def recibe():
     i = 0
     while not evento_salida.is_set():  
         
-        try:
-            rawString = arduino.readline()
-            array_serial = np.fromstring(rawString, dtype=float, count=-1, sep=',')
-            
-#            rawString = arduino.read(4*cant_variables)
-#            array_serial = struct.unpack(cant_variables*'f',rawString)
+        try:            
+            rawString = arduino.read(4*cant_variables*cant_chunk_rec)
+            array_serial = struct.unpack(cant_variables*cant_chunk_rec*'f',rawString)
             
             now = datetime.datetime.now()
             now = now.strftime("%Y-%m-%d %H:%M:%S.%f")  
             
-            if len(array_serial) == cant_variables:
+            if len(array_serial) == cant_variables*cant_chunk_rec:
                 buffer_in_data[i,:] = array_serial
                 buffer_in_timestamp[i] = now
                 i = i+1
                 i = i%buffer_in_data.shape[0]
                 semaphore1.release()
-
-                
-                #print(i,now,array_serial)
             
         except:
-            print("Exiting")
-            #break    
+            print("Error en la lectura")
         
-        #print(arduino.inWaiting())
         while arduino.inWaiting() > 200:
-            print('ava')
+            print('Limpiando buffer')
             arduino.readline()
         
 
-#        try:
-#            setpoint = 0.1
-#            kp = 0.2
-#            ki = 0.7
-#            kd = 0.5
-#            isteps = 1.
-#            
-#            
-#            arduino.write(struct.pack('<fffff',setpoint,kp,ki,kd,isteps))
-#            print(arduino.writable())
-##            arduino.reset_output_buffer()
-##            arduino.flush()    
-#        except:
-#            print("Error")
+
         
 def manda():   
 
-    setpoint = 1.2
+    setpoint = 2000
     kp = 0.1
     ki = 0.2
     kd = 0.3
     isteps = 20.   
     
     while not evento_salida.is_set():     
-        time.sleep(1)
+        time.sleep(2)
         try:
-            setpoint += 0.1
-            kp += 0.2
-            ki += 0.7
-            kd += 0.5
-            isteps += 1.
-            
-            
-            arduino.write(struct.pack('<fffff',setpoint,kp,ki,kd,isteps))
-            #print(arduino.writable())
-#            arduino.reset_output_buffer()
-#            arduino.flush()    
+             
+            arduino.write(struct.pack('<fffff',setpoint,kp,ki,kd,isteps)) 
         except:
-            print("Error")
+            print("Error en el envio")
             
 
 
@@ -114,11 +92,7 @@ def grafica():
     i = 0
     while not evento_salida.is_set(): 
 
-
-#def grafica():
-#    global data,j
-
-        
+  
         semaphore1.acquire()
             
         if not i%sub_chunk_plot:
@@ -131,7 +105,7 @@ def grafica():
             #data[0:-1] = data[1:]
             #data[data.shape[0]-1] = buffer_in_data[i,8]
             
-            data = np.append(data,buffer_in_data[j:jj,8])
+            data = np.append(data,buffer_in_data[j:jj,5])
             data = data[sub_chunk_plot:]
             
             line.set_ydata(data)  
@@ -140,16 +114,14 @@ def grafica():
         i = i%buffer_in_data.shape[0]
         
         fig.canvas.draw_idle()
-        #return line,
     
 
 data = np.zeros(buffer_in_data.shape[0])  
-#j = 0  
         
 fig = plt.figure(figsize=(7,3.7),dpi=250)
 ax = fig.add_axes([.15, .15, .70, .70])  
-line, = ax.plot(data, '-')      
-ax.set_ylim([0,5000])
+line, = ax.plot(data, '.')      
+ax.set_ylim([-200,200])
 
 t1 = threading.Thread(target=recibe, args=[])
 t2 = threading.Thread(target=manda, args=[])
@@ -158,3 +130,35 @@ t1.start()
 t2.start()
 t3.start()
 
+
+
+#%%
+evento_salida.set()
+time.sleep(1)
+arduino.close()
+
+#%%
+
+plt.plot(np.diff(buffer_in_data[:,10]),'.')
+plt.ylim([0,100])
+
+plt.plot(buffer_in_data[:,0],'.')
+plt.ylim([0,5000])
+
+plt.plot(buffer_in_data[:,4],'.')
+plt.ylim([0,200])
+
+plt.plot(buffer_in_data[:,4],'.')
+plt.plot(buffer_in_data[:,5],'.')
+plt.ylim([-1000,1000])
+
+plt.plot(buffer_in_data[:,5],'-',alpha=0.6)
+plt.plot(buffer_in_data[:,7],'-',alpha=0.6)
+plt.ylim([-1000,1000])
+
+plt.plot(buffer_in_data[:,6],'-',alpha=0.6)
+plt.ylim([-1000,1000])
+
+plt.plot(np.diff(buffer_in_data[:,5])*420)
+plt.plot(buffer_in_data[:,7])
+plt.ylim([-20000,20000])
--- Arduino/v3/v3.ino
+++ Arduino/v3/v3.ino
@@ -3,9 +3,8 @@
 #define SERIAL_BUFFER_SIZE 2048
 #define out_chunks 5000
 #define samples_per_chunk 100
-#define chunk_to_python 200
-#define chunk_from_python 100
-#define nbr_out_variables 10
+#define chunk_to_python 20
+#define nbr_out_variables 11
 
 volatile float buffer_in[samples_per_chunk];
 volatile float error_buffer_out[out_chunks];
@@ -16,23 +15,17 @@ volatile int p = 0;
 volatile long k = 0;
 
 // Terminos PID
-volatile float termino_p = 0.0;
-volatile float termino_i = 0.0;
-volatile float termino_d = 0.0;
+float termino_p = 0.0;
+float termino_i = 0.0;
+float termino_d = 0.0;
 
 float setpoint = 2.3;
 float kp = 1.0;
 float ki = 5.0;
 float kd = 4.05;
 float isteps  = 200;
-
-float setpoint_rec = 2.3;
-float kp_rec = 1.0;
-float ki_rec = 5.0;
-float kd_rec = 4.05;
-float isteps_rec  = 200;
-
-volatile float dt = 0.0;
+float isteps_ant = 0;
+float dt = 0.0;
 
 ///////////////////////////
 ///////////////////////////
@@ -45,24 +38,18 @@ int recive_from_python_pin_in = 4;
 int clock_pin_out = 35;
 int chunk_pin_out = 37;
 int send_to_python_pin_out = 39;
-int recive_from_python_pin_out = 41;
 int analog_read_pin = 0;
 
-float clock_frequency = 40000;
+float clock_frequency = 160000;
 int clock_ind = 0;
 float chunk_frequency = 0.;
 long chunk_ind = 0;
 float send_to_python_frequency = 0.;
 long send_to_python_ind = 0;
-float recive_from_python_frequency = 0.;
-long recive_from_python_ind = 0;
 
 int m = 0;
 int q = 0;
-
-
 float dummy = 0.;
-
 int vali = 0;
 
 
@@ -75,7 +62,7 @@ void setup() {
   analogWriteResolution(12);
   pinMode (DAC0,OUTPUT);
   pinMode (DAC1,OUTPUT);
-  analogWrite(DAC1, 3000);  
+  analogWrite(DAC1, 2010);  
 
   
   pinMode (clock_pin_in, INPUT);
@@ -86,8 +73,8 @@ void setup() {
   pinMode(chunk_pin_out, OUTPUT);
   pinMode(send_to_python_pin_out, OUTPUT);
   
-
-  int divi = 500;
+  // Setea las frecuencias de los clocks
+  int divi = 100;
   clock_ind = round(84000000/divi/clock_frequency);
   clock_frequency = round(84000000/divi)/clock_ind;
   chunk_frequency = clock_frequency/samples_per_chunk;
@@ -96,9 +83,6 @@ void setup() {
   send_to_python_frequency = clock_frequency/samples_per_chunk/chunk_to_python;
   send_to_python_ind = round(84000000/divi/send_to_python_frequency);
   send_to_python_frequency = round(84000000/divi)/send_to_python_ind;
-  recive_from_python_frequency = clock_frequency/samples_per_chunk/chunk_from_python;
-  recive_from_python_ind = round(84000000/divi/recive_from_python_frequency);
-  recive_from_python_frequency = round(84000000/divi)/recive_from_python_ind;
 
   dt = 1./chunk_frequency;
 
@@ -119,27 +103,19 @@ void setup() {
   Serial.print("chunk_frequency/send_to_python_frequency: ");
   Serial.println(chunk_frequency/send_to_python_frequency);
 
-  Serial.print("recive_from_python_frequency: ");
-  Serial.println(recive_from_python_frequency);
-  Serial.print("recive_from_python_ind: ");
-  Serial.println(recive_from_python_ind);
-
-  
   Serial.print("buffer_size: ");
   Serial.println(SERIAL_BUFFER_SIZE);
 
   delay(1000);
 
+
+  // Seteo los clocks
   REG_PIOC_PDR = 0x3FC;  //B1111111100, PIO Disable Register
   REG_PIOC_ABSR = REG_PIOC_ABSR | 0x3FCu; //B1111111100, Peripheral AB Select Register
   REG_PMC_PCER1 = REG_PMC_PCER1 | 16;
   REG_PWM_ENA = REG_PWM_SR | B1111;
   REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(divi);    // Set the PWM clock rate to 2MHz (84MHz/42). Adjust DIVA for the resolution you are looking for                                                     
 
-  REG_PWM_CMR3 = PWM_CMR_CALG |PWM_CMR_CPRE_CLKA;      // The period is left aligned, clock source as CLKA on channel 2 - PIN39
-  REG_PWM_CPRD3 = recive_from_python_ind;                             // Channel 2 : Set the PWM frequency 2MHz/(2 * CPRD) = F ; 1< CPRD < 2exp24  -1 ; here CPRD = 10 exp 6
-  REG_PWM_CDTY3 = round(recive_from_python_ind*0.5); 
-  
   REG_PWM_CMR2 = PWM_CMR_CALG |PWM_CMR_CPRE_CLKA;      // The period is left aligned, clock source as CLKA on channel 2 - PIN39
   REG_PWM_CPRD2 = send_to_python_ind;                             // Channel 2 : Set the PWM frequency 2MHz/(2 * CPRD) = F ; 1< CPRD < 2exp24  -1 ; here CPRD = 10 exp 6
   REG_PWM_CDTY2 = round(send_to_python_ind*0.5); 
@@ -152,7 +128,7 @@ void setup() {
   REG_PWM_CPRD0 = clock_ind;                             // Channel 2 : Set the PWM frequency 2MHz/(2 * CPRD) = F ; 1< CPRD < 2exp24  -1 ; here CPRD = 10 exp 6
   REG_PWM_CDTY0 = round(clock_ind*0.5); 
   
-
+  // Inicializo buffer de medicion y de error
   for (k=0;k<samples_per_chunk;k++){
       buffer_in[k] = 0.0;
   }  
@@ -161,34 +137,23 @@ void setup() {
       error_buffer_out[m] = 0.0;
   }      
 
-
-  //Serial.readBytes((byte*) buffer_acq, sizeof(buffer_acq));
-
   k = 0;
   q = 0;
   n = 0;  
   m = 0;
   p = 0;
 
-
-
+  // Mando los callbacks
   attachInterrupt(digitalPinToInterrupt(clock_pin_in), acq_callback, RISING); 
-  attachInterrupt(digitalPinToInterrupt(chunk_pin_in), chunk_callback, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(chunk_pin_in), chunk_callback, RISING); 
   attachInterrupt(digitalPinToInterrupt(send_to_python_pin_in), send_to_python_callback, FALLING); 
-  //attachInterrupt(digitalPinToInterrupt(recive_from_python_pin_in), recive_from_python_callback, FALLING); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-//  Serial.print("hola");
-//  delay(10);
-
-    char to_python_serial[200];
-    sprintf(to_python_serial, "%f, %f ,%f, %f, %f, %f, %f, %f, %f, %f \n",variables_buffer_out[0], variables_buffer_out[1], variables_buffer_out[2], variables_buffer_out[3], variables_buffer_out[4], variables_buffer_out[5], variables_buffer_out[6], variables_buffer_out[7], variables_buffer_out[8], variables_buffer_out[9]);
-    Serial.write(to_python_serial);
-
+    // Recibe de python
     if(Serial.available() > 50){ 
         Serial.readBytes((char*)&setpoint, sizeof(setpoint));
         Serial.readBytes((char*)&kp, sizeof(kp));
@@ -212,31 +177,49 @@ void acq_callback(){
 void chunk_callback(){
 
     int i = 0;
+    int w = 0;
+
+    // Promedio de la medicion
     long suma = 0;
     for (i=0;i<samples_per_chunk;i++){
         suma += buffer_in[i];   
     }
     float avg = suma/samples_per_chunk;
 
-    // Terminos PID
+    // Error
     error_buffer_out[n] = setpoint - avg;
   
-    // BUffer circular 
+    // Buffer circular 
     int jj = 0;
     int j  = 0;
     jj = (n-int(isteps) + out_chunks)%out_chunks;
-    j = (n-1 + out_chunks)%out_chunks;;
+    j = (n-1 + out_chunks)%out_chunks;
   
     termino_p = error_buffer_out[n];
     termino_i += (error_buffer_out[n] - error_buffer_out[jj])*dt;
     termino_d = (error_buffer_out[n] - error_buffer_out[j])/dt;
+
+    // Calcula el termino i
+    if (isteps_ant != isteps){
+      termino_i = 0.;
+      if (jj > n){
+        for (w=jj;w<out_chunks;w++){
+          termino_i += error_buffer_out[w];
+          }
+        for (w=0;w<n;w++){
+          termino_i += error_buffer_out[w];
+          }               
+        }else{
+          for (w=jj;w<n;w++){
+            termino_i += error_buffer_out[w];
+            }                 
+          }
+          termino_i = termino_i*dt;
+      }
     
     float control = kp*termino_p + ki*termino_i + kd*termino_d;
 
-    vali += 1;
-    analogWrite(DAC1, vali/2); 
-
-
+    // Guarda el vector de salida
     variables_buffer_out[0] = setpoint;
     variables_buffer_out[1] = kp;
     variables_buffer_out[2] = ki;
@@ -247,74 +230,22 @@ void chunk_callback(){
     variables_buffer_out[7] = termino_d;
     variables_buffer_out[8] = avg;
     variables_buffer_out[9] = control;    
-    
-
-//    Serial.print(avg);
-//    if (n==0){
-//      Serial.println()
-//      }
-
-//    if (not n%out_chunks){
-//      Serial.println(q);
-//      q = q+1;
-//      }
-
-  if (not n){
-      //Serial.println(vali);
-      vali = 0;
-    }
+    variables_buffer_out[10] = float(n);     
 
     n = n + 1;
     n = n%out_chunks;
 
+    isteps_ant = isteps;
 
 }
 
 void send_to_python_callback(){
 
-
-    char to_python_serial[200];
-    sprintf(to_python_serial, "%f, %f ,%f, %f, %f, %f, %f, %f, %f, %f \n",variables_buffer_out[0], variables_buffer_out[1], variables_buffer_out[2], variables_buffer_out[3], variables_buffer_out[4], variables_buffer_out[5], variables_buffer_out[6], variables_buffer_out[7], variables_buffer_out[8], variables_buffer_out[9]);
-    Serial.write(to_python_serial);
-
-    //Serial.write( (byte*)&variables_buffer_out, sizeof(variables_buffer_out) );
-
-
-  if (not q){
-      if(Serial.available() >= 20){ 
-          Serial.readBytes((char*)&setpoint, sizeof(setpoint));
-          Serial.readBytes((char*)&kp, sizeof(kp));
-          Serial.readBytes((char*)&ki, sizeof(ki)); 
-          Serial.readBytes((char*)&kd, sizeof(kd));
-          Serial.readBytes((char*)&isteps, sizeof(isteps));  
-        }
-
-        while (Serial.available()){
-          Serial.readBytes((char*)&dummy, sizeof(dummy));
-          }
-        
-//        setpoint = setpoint_rec;
-//        kp = kp_rec;
-//        ki = ki_rec;
-//        kd = kd_rec;
-//        isteps = isteps_rec;
-  }
-
-  Serial.flush();
-
- q = q+1;
- q = q%100;
-
- 
+    Serial.write( (byte*)&variables_buffer_out, sizeof(variables_buffer_out) );
 
 }
 
 
-void recive_from_python_callback(){
-
-
-
-}  
 
   
 
